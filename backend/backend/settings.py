adce83b3c5a3c7be53ed72b2d091e8b57f55f6e2
ABashir01 <ahadbashirblast@gmail.com>
2024-08-12 11:59:47 -0500

    Testing dummy backend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -80,12 +80,11 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.dummy',
+    }
+}
 
 
 # Password validation
